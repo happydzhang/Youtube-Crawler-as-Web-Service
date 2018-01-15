4fe62b122ab890c26c099b48aaca64fef8075e99
happydzhang <happydzhang@gmail.com>
2018-01-15 15:21:52 -0500

    Add files via upload

---
+++ pipeline.py
@@ -0,0 +1,92 @@
+import sys
+import logging
+
+import threading
+from collectData import *
+from selenium import webdriver
+
+def usage(status):
+    print "Usage: ./collectData [keyword] [n] [t]"
+    print ""
+    print "     - keyword needs to be in quotations"
+    print "     - n is the top n links in search result (< 5)"
+    print "     - t is the time out in minutes"
+
+    sys.exit(status)
+
+
+def get_links(keyword, number,driver_path):
+  
+    # Set url
+    keyword = keyword.replace(' ', '+')
+    url = "https://www.youtube.com/"
+    url = url + "results?search_query=" + keyword # search query
+    url = url + "&sp=EgJAAVAU" # live stream filter
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+
+    driver = webdriver.Chrome(driver_path+"/chromedriver",chrome_options=options)
+    driver.get(url)
+
+    videos = driver.find_elements_by_tag_name('ytd-video-renderer')
+
+    # Store links in list
+    links = []
+    n = 0
+    for video in videos:
+        thumb = video.find_element_by_id('thumbnail')
+        link = thumb.get_attribute('href').encode('utf-8')
+        links.append(link)
+
+        n = n + 1
+        if n == number:
+            break
+
+    driver.close()
+
+    return links
+
+
+def pipeline_run(links):
+    driver_path = os.getcwd()
+    # Parse command line argument
+    #if len(sys.argv) != 4:
+    #    usage(1)
+
+    #keyword = sys.argv[1]
+    #number = int(sys.argv[2])
+    #timeout = int(sys.argv[3])
+    #keyword = kw
+    number = 2
+    timeout = 10
+    #if number < 1 or number > 4:
+    #    usage(1)
+
+    #links = list()
+    #links = get_links(keyword,number,driver_path)
+
+    # Set flag and lock for shutdown
+    shutdown = False
+    threads = []
+
+    # Start threads to crawl data
+    for link in links:
+        print("Starting thread for link: " + link)
+        t = threading.Thread(target = crawl_link, args=(link, lambda: shutdown,))
+        threads.append(t)
+        t.start()
+
+    # This blocks specified timeout
+    print("Beginning countdown...")
+    time.sleep(timeout*60)
+
+    # Change flag to signal threads to shutdown
+    shutdown = True
+    print("Preparing to shutdown...")
+
+    for t in threads:
+        t.join()
+
+    sys.exit(0)
---
+++ webserver.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import sys
+import logging
+import threading
+from collectData import *
+from pipeline import *
+from selenium import webdriver
+
+app = Flask(__name__)
+
+@app.route('/args')
+def render():
+    if 'kw' in request.args:
+    	driver_path = os.getcwd()
+        keywords = request.args.get('kw')
+        keywords = keywords.replace('.',' ')
+        print keywords
+        links = list()
+    	links = get_links(keywords,2,driver_path)
+        pipeline_run(links) 
+        return keywords
+    else:
+        return "No input specified"
+
+if __name__=='__main__':
+    app.run(debug=True)
+#http://localhost:5000/args?kw=nba.houston.rockets
