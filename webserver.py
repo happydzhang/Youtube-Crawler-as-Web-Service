1ea9a985c086a1cb32b11e9ae6d8ac17544a2c54
happydzhang <happydzhang@gmail.com>
2018-01-15 15:37:52 -0500

    Add files via upload

--- webserver.py
+++ webserver.py
@@ -11,12 +11,11 @@ app = Flask(__name__)
 @app.route('/args')
 def render():
     if 'kw' in request.args:
-    	driver_path = os.getcwd()
+        driver_path = os.getcwd()
         keywords = request.args.get('kw')
         keywords = keywords.replace('.',' ')
-        print keywords
         links = list()
-    	links = get_links(keywords,2,driver_path)
+        links = get_links(keywords,2,driver_path)
         pipeline_run(links) 
         return keywords
     else:
